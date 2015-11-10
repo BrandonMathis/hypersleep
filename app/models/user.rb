3f19f50bf4ae24235564e82335a177107c0cf542
Brandon Mathis <bemathis@gmail.com>
2015-11-10 18:14:24 -0500

    Persisting token

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ActiveRecord::Base
          :omniauthable
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user = where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
+    user.github_token = auth['credentials']['token']
+    user.save!
+    user
   end
 end
